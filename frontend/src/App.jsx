3f60ca46e9f22e1c671d309bdc76d0262aa74388
bananaFish001 <raj123.rs308@gmail.com>
2024-04-29 23:34:58 +0530

    new app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>coffee and train</h1>
+    <p>JOKES: {jokes.length}</p>
+    {
+      jokes.map((jokes, index) => {
+        <div key={jokes.id}>
+        <h3>{jokes.title}</h3>
+        <p>{jokes.content}</p>
+        </div>
+      })
+    }
+    </>
+  )
+}
+
+export default App
